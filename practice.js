b2d86c91ad82d9657f1236818dda9047d5282162
WillHoll <willhollingsworth15@yahoo.com>
2019-10-01 13:26:15 -0600

    problem 9

--- practice.js
+++ practice.js
@@ -53,7 +53,7 @@ let newGreeting = function(name) {
 //Create an array called groceries with the values
 //"apples", "milk", "eggs", "bread"
 
-//Code Here
+let groceries = ["apples", "milk", "eggs", "bread"];
 
 //////////////////PROBLEM 9////////////////////
 
@@ -62,7 +62,12 @@ let newGreeting = function(name) {
 //name (a string), color (a string), age (a number),
 //and goodBoy (a boolean).
 
-//Code Here
+let dog = {
+  name: 'Buster',
+  color: "light brown",
+  age: 5,
+  goodBoy: true
+}
 
 //...access the dog's name from the object and assign it to a
 //variable called devMountainClassPet.
